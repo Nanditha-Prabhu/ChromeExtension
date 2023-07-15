eec228d3c1ca0df227baccde85388f0731f23f21
Nandithu <nanditha7prabhu@gmail.com>
2023-07-16 02:54:29 +0530

    event listener

---
+++ index.js
@@ -0,0 +1,5 @@
+let inputBtn = document.getElementById("input-btn")
+// instead of onclick we use this for cleaner separation of html and js code
+inputBtn.addEventListener("click", function() {
+    console.log("button clicked!")
+})
