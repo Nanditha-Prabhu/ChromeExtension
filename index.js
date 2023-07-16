4905b4080f1a894f4334a06a685d9522e25344e5
Nandithu <nanditha7prabhu@gmail.com>
2023-07-17 01:15:40 +0530

    ip btn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+
 // instead of onclick we use this for cleaner separation of html and js code
 inputBtn.addEventListener("click", function() {
-    console.log("button clicked!")
+    myLeads.push(inputEl.value);
+    console.log(myLeads)
 })
