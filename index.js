361d806ca5c7f5d8f3aefa049f50186f8ba2eacf
M Nanditha Prabhu <nanditha7prabhu@gmail.com>
2023-10-11 23:38:43 +0530

    listing elements

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let myLeads = []
+let myLeads = ['www.coolors.co', 'www.unsplash.com']
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 
 // instead of onclick we use this for cleaner separation of html and js code
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads)
+    console.log(myLeads);
 })
+
+for(let i=0; i<myLeads.length; i++){
+    ulEl.innerHTML += '<li>'+ myLeads[i] + '</li>';
+}
