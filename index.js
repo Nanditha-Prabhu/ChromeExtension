bdc47a7d14c6b58bfb079a5fcd45c8351466e741
M Nanditha Prabhu <nanditha7prabhu@gmail.com>
2023-10-15 21:40:29 +0530

    final changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ['https://www.coolors.co', 'https://www.unsplash.com']
+let myLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
