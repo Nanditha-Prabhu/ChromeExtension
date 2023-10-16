2cbb875eb1d9dfe57fe3250470e27a6f2aef689c
M Nanditha Prabhu <nanditha7prabhu@gmail.com>
2023-10-17 00:56:32 +0530

    selling

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-
 // get the leads variable from local storage. change its type from strings to original form-array
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+// this is const because rule says, if you can use const then use const because it instills strictness in the code
 
 // if there exists values stored in myleads variable then set call it when refreshed again
 if ( leadsFromLocalStorage ){
-    myLeads = leadsFromLocalStorage
-    renderLeads()
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
+
+// clear storage if double clicked on delete all
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
 
 // instead of onclick we use this for cleaner separation of html and js code
 inputBtn.addEventListener("click", function() {
     // add it to the list
     myLeads.push(inputEl.value);
     // clear the input field after saving 
-    inputEl.value = ""
+    inputEl.value = "";
     // save myleads to local storage
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
     // to verify its working
     // console.log( localStorage.getItem('myLeads'))
     //display the list
-    renderLeads()
+    render(myLeads);
+    // the object passed here is argument(outside fn)
 })
 
-function renderLeads(){
-    let leadItems = ""
-    for(let i=0; i<myLeads.length; i++){
+// the object passed here is parameter(inside fn)
+function render(itemList){
+    let Items = ""
+    for(let i=0; i<itemList.length; i++){
         // normal html
         // leadItems += '<li><a href="https://'+myLeads[i]+'" target="_blank">'+ myLeads[i] + '</a></li>';
 
         // using template strings/ template literals with back ticks
         // this makes code more cleaner and allows to wrrite in multiple lines
-        leadItems +=`<li>
-                            <a href='${myLeads[i]}' target="_blank">
-                                ${myLeads[i]}
+        Items +=`<li>
+                            <a href='${itemList[i]}' target="_blank">
+                                ${itemList[i]}
                             </a>
                      </li>`;
                      // note: we must add https:// in our url(myLeads) itself.
     }
-    ulEl.innerHTML = leadItems
+    ulEl.innerHTML = Items
 }
